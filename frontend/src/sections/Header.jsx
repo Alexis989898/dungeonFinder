3fc709bf941c1494f1dc9023868feb92118f0ce4
Ian Roland <ianrolandnascimento@gmail.com>
2024-11-11 08:54:50 -0300

    Fix: the header bug on mobile was solved (still missing background on mobile)

--- frontend/src/sections/Header.jsx
+++ frontend/src/sections/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
     <>
       <header
         className={clsx(
-          "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500",
+          "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
           hasScrolled && "py-2 bg-slate-950 backdrop-blur-[8px]"
         )}
       >
